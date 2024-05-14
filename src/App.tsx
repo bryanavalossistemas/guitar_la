5c784f0c25f7cd04334dfff4e7c75e9189d418ad
bryan <bryanavalossistemas@gmail.com>
2024-05-14 00:31:53 -0500

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -40,7 +40,7 @@ function App() {
       <footer className="bg-dark mt-5 py-5">
         <div className="container-xl">
           <p className="text-white text-center fs-4 mt-4 m-md-0">
-            GuitarLA - Todos los derechos Reservados
+            GuitarLA - Todos los derechos Reservados @2024
           </p>
         </div>
       </footer>
